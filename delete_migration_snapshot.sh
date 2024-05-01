b50bc1e600fd1bdce16c3f1c0f629243dc2b2438
becatini <19430469+becatini@users.noreply.github.com>
2024-05-01 17:34:19 -0600

    .

--- delete_migration_snapshot.sh
+++ delete_migration_snapshot.sh
@@ -7,19 +7,20 @@ get_date_time() {
     date +%Y-%m-%d" "%H:%M
 }
 
-# List all active accounts in the organization
-#for account in $(aws organizations list-accounts --query 'Accounts[?Status==`ACTIVE`].Id' --profile master --output text);
 account=$1
-do
+
+# List all active accounts in the organization
+#for account in $(aws organizations list-accounts --query 'Accounts[?Status==`ACTIVE`].Id' --profile master --output text); do
 
     # Global variables
     current_date=$(date +%Y-%m-%d)
-    full_log="full_log_${account}_${current_date}.txt"
+    full_log="full_log_delete_snapshot_${current_date}.txt"
 
+    echo "$(get_date_time)" | tee -a $full_log
     echo "" | tee -a $full_log
     echo "+------------------------------+" | tee -a $full_log
     echo "Processing account: $account"     | tee -a $full_log
-    echo "+------------------------------+" | tee -a $full_log
+    echo "+------------------------------+" | tee -a $full_log    
        
     # Assume role Terraform
     rolearn="arn:aws:iam::$account:role/Terraform"
@@ -43,27 +44,24 @@ do
         echo "Processing region: $region" | tee -a $full_log
 
         # Get all snapshots with description "Migrate gp2 to gp3"
-        snapshots=$(aws ec2 describe-snapshots 
-                        --region $region 
-                        --owner-ids $account 
-                        --filters Name=description,Values="Migrate gp2 to gp3" 
-                        --query 'Snapshots[*].SnapshotId' 
+        snapshots=$(aws ec2 describe-snapshots \
+                        --region $region \
+                        --owner-ids $account \
+                        --filters Name=description,Values="Migrate gp2 to gp3" \
+                        --query 'Snapshots[*].SnapshotId' \
                         --output text)
 
         # Check if there is any pre-migration snapshots
         if [ -z "$snapshots" ]; then
-            echo "Region $region has no pre-migration snapshots" | tee -a $full_log
+            echo "No pre-migration snapshots" | tee -a $full_log
 
-        else
-            echo "$(get_date_time)" | tee -a $full_log
+        else            
             echo "Deleting snapshot ID..." | tee -a $full_log
             # Read the snapshots
-            echo "$snapshots" | while read -r line; do
-                
-                snapshot_id=$(echo "$line" | awk '{print $1}')
-                
+            # echo "$snapshots" | while read -r line; do
+            for snapshot_id in $(echo "${snapshots}"); do
                 # Delete snapshot
-                echo "${snapshot_id}"
+                echo "${snapshot_id}" | tee -a $full_log
                 aws ec2 delete-snapshot --region $region --snapshot-id ${snapshot_id}                
             done
         fi
@@ -73,4 +71,4 @@ do
     unset AWS_ACCESS_KEY_ID
     unset AWS_SECRET_ACCESS_KEY
     unset AWS_SESSION_TOKEN
-done
+# done
