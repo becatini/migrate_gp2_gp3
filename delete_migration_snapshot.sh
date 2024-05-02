839bd58f1643a7805dbd91d62f527fa220871e7e
becatini <19430469+becatini@users.noreply.github.com>
2024-05-02 15:33:18 -0600

    .

--- delete_migration_snapshot.sh
+++ delete_migration_snapshot.sh
@@ -7,14 +7,14 @@ get_date_time() {
     date +%Y-%m-%d" "%H:%M
 }
 
-account=$1
+> accounts.txt
 
-# List all active accounts in the organization
-#for account in $(aws organizations list-accounts --query 'Accounts[?Status==`ACTIVE`].Id' --profile master --output text); do
+# Global variables
+current_date=$(date +%Y-%m-%d)
+full_log="full_log_delete_snapshot_${current_date}.txt"
 
-    # Global variables
-    current_date=$(date +%Y-%m-%d)
-    full_log="full_log_delete_snapshot_${current_date}.txt"
+# Get all active accounts in the organization
+for account in $(aws organizations list-accounts --query 'Accounts[?Status==`ACTIVE`].Id' --profile master --output text); do
 
     echo "$(get_date_time)" | tee -a $full_log
     echo "" | tee -a $full_log
@@ -34,41 +34,47 @@ account=$1
     export AWS_SECRET_ACCESS_KEY=$(echo $assumed_role | jq -r '.SecretAccessKey')
     export AWS_SESSION_TOKEN=$(echo $assumed_role | jq -r '.SessionToken')
 
-    # Get AWS regions
-    aws_regions=$(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
-
-    # Loop to work on all regions
-    for region in $aws_regions; do
-
-        echo "" | tee -a $full_log
-        echo "Processing region: $region" | tee -a $full_log
-
-        # Get all snapshots with description "Migrate gp2 to gp3"
-        snapshots=$(aws ec2 describe-snapshots \
-                        --region $region \
-                        --owner-ids $account \
-                        --filters Name=description,Values="Migrate gp2 to gp3" \
-                        --query 'Snapshots[*].SnapshotId' \
-                        --output text)
-
-        # Check if there is any pre-migration snapshots
-        if [ -z "$snapshots" ]; then
-            echo "No pre-migration snapshots" | tee -a $full_log
-
-        else            
-            echo "Deleting snapshot ID..." | tee -a $full_log
-            # Read the snapshots
-            # echo "$snapshots" | while read -r line; do
-            for snapshot_id in $(echo "${snapshots}"); do
-                # Delete snapshot
-                echo "${snapshot_id}" | tee -a $full_log
-                aws ec2 delete-snapshot --region $region --snapshot-id ${snapshot_id}                
-            done
-        fi
-    done
+    if [ -z "${assumed_role}" ]; then
+        echo "${account} NOK" >> accounts.txt
+    else
+        echo "${account} OK" >> accounts.txt
+    
+		# Get AWS regions
+		aws_regions=$(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
+	
+		# Loop to work on all regions
+		for region in $aws_regions; do
+	
+			echo "" | tee -a $full_log
+			echo "Processing region: $region" | tee -a $full_log
+	
+			# Get all snapshots with description "Migrate gp2 to gp3"
+			snapshots=$(aws ec2 describe-snapshots \
+							--region $region \
+							--owner-ids $account \
+							--filters Name=description,Values="Migrate gp2 to gp3" \
+							--query 'Snapshots[*].SnapshotId' \
+							--output text)
+	
+			# Check if there is any pre-migration snapshots
+			if [ -z "$snapshots" ]; then
+				echo "No pre-migration snapshots" | tee -a $full_log
+	
+			else            
+				echo "Deleting snapshot ID..." | tee -a $full_log
+				# Read the snapshots
+				# echo "$snapshots" | while read -r line; do
+				for snapshot_id in $(echo "${snapshots}"); do
+					# Delete snapshot
+					echo "${snapshot_id}" | tee -a $full_log
+					aws ec2 delete-snapshot --region $region --snapshot-id ${snapshot_id}                
+				done
+			fi
+		done
+    fi
 
     # Unset the assumed role credentials
     unset AWS_ACCESS_KEY_ID
     unset AWS_SECRET_ACCESS_KEY
     unset AWS_SESSION_TOKEN
-# done
+done
